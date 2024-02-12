7c9cf15c17852ad6629cf30391e657cf59c91e42
Sudipta-Chakraborty <sudipta1436@gmail.com>
2024-02-12 12:31:56 +0530

    add

---
+++ chat-ai-static/static/options.js
@@ -0,0 +1,8 @@
+let options = {
+  Eureka: 'eureka is a service. read more to click on this link <a href="https://aws.amazon.com/inspector/features/?nc=sn&loc=2" target="_blank">abc</a><br>',
+  Consul: '<a href="https://www.consul.io/consul-on-kubernetes">consul provide autodiscover.read more to click on this link.xyz</a><br>',
+  Trafeik: "Traefik (pronounced like traffic) is a modern HTTP reverse proxy and load balancer made to deploy microservices with ease",
+};
+
+
+
---
+++ chat-ai-static/static/q_a.js
@@ -0,0 +1,5 @@
+let qa = {
+  "good morning": "hi, good morning, how can i help you sir",
+   "Amazon Inspector": '<a href="https://aws.amazon.com/inspector/features/?nc=sn&loc=2" target="_blank">Amazon Inspector Features</a><br>'
+
+};
